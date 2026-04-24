8e85ce23687aac1d307b40eb081aeea5b03a9583
TioKoko05 <Machucaverso25@gmail.com>
2026-04-24 14:55:52 -0400

    Materia actualizada

--- For.py
+++ For.py
@@ -11,10 +11,24 @@
 # for i in range(10):
 #     print(f"{num} X {i+1} = {num*(i+1)}")
 
-notas=int(input("Ingrese un el total de pruebas que tiene: "))
-suma=0
-for i in range(notas):
-    n=float(input(f"ingrese su {i+1}° nota: "))
-    suma=suma+n
-prom=suma/notas
-print(f"El promedio de sus notas es: {round(prom)}")
+# notas=int(input("Ingrese el total de pruebas que tiene: "))
+# suma=0
+# for i in range(notas):
+#     n=float(input(f"ingrese su {i+1}° nota: "))
+#     suma=suma+n
+# prom=suma/notas
+# print(f"El promedio de sus notas es: {round(prom)}")
+
+# num=int(input("Ingrese un numero: "))
+# total=0
+# for i in range(1, num+1):
+#     total+=i
+# print(f"El resultado es {total}")
+
+# Factorial
+
+num=int(input("Ingrese un numero: "))
+total=0
+for i in range(1, num+1):
+    total+=i
+print(f"El resultado es {total}")
