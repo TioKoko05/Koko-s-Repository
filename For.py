ffa4f931cbd8f4d2f3f3fc77e267abeb7585a53e
TioKoko05 <Machucaverso25@gmail.com>
2026-04-24 14:23:01 -0400

    Nueva materia (For)

---
+++ For.py
@@ -0,0 +1,20 @@
+# Explicacion y uso de FOR
+
+# for i in range(5):
+#     print(i+1)
+
+# num=int(input("Ingrese un numero"))
+# for i in range(1,num+1):
+#     print("repeticion", i)
+
+# num=int(input("Ingrese un numero para ver tabla de multiplicacion hasta el 10: "))
+# for i in range(10):
+#     print(f"{num} X {i+1} = {num*(i+1)}")
+
+notas=int(input("Ingrese un el total de pruebas que tiene: "))
+suma=0
+for i in range(notas):
+    n=float(input(f"ingrese su {i}° nota: "))
+    suma=suma+n
+prom=suma/notas
+print(f"El promedio de sus notas es: round{notas/num}")
